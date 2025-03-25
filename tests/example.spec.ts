58b4e8fa3b36629c1ff27b4930376704cba5a596
Nguyen <phucnguyen439@gmail.com>
2025-03-25 20:12:24 +0700

    ex1

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -11,8 +11,8 @@ test('get started link', async ({ page }) => {
   await page.goto('https://material.playwrightvn.com/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Bài học 1: Register Page' }).click();
+  await page.getByRole('link', { name: 'Bài học 2: Product page' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'User Registration' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Simple E-commerce' })).toBeVisible();
 });
